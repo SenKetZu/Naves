6c2ec329dc7aff7feaeab15e9db2c153e50dfc76
PC <fahler114@gmail.com>
2021-03-02 17:56:15 -0300

    model-test
    probando modelos para la nave

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform pj;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = pj.position + offset;
+    }
+}
