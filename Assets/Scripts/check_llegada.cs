cf18c20e6f109b0d82643e3fc96dce002e1f96bc
PC <fahler114@gmail.com>
2021-03-02 21:33:58 -0300

    plataform-test
    cree un script para la plataforma para que cambie de color al colisonar con la nave

---
+++ Assets/Scripts/check_llegada.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class check_llegada : MonoBehaviour
+{
+    public Material material;
+    public MeshRenderer mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        mesh.material = material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
